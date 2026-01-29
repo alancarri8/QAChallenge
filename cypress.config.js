8e081195b37d58569924280e6db4cf8ce91fcbb6
alancarri8 <carrizoalan6@gmail.com>
2026-01-28 23:07:27 -0300

    Se agrega configuración de reporte

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Challenge QA",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      screenshotOnRunFailure: true;
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
