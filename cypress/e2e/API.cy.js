f0e1dc58758a2409eefe96a96144830ab37b924a
alancarri8 <carrizoalan6@gmail.com>
2026-01-29 11:26:58 -0300

    Se modifica comando para correr api

--- cypress/e2e/API.cy.js
+++ cypress/e2e/API.cy.js
@@ -1,11 +1,10 @@
-describe('Prueba de API MercadoLibre', () => {
+describe("Prueba de API MercadoLibre", () => {
   it('Validar que el cuerpo de la respuesta contenga el campo "departments"', () => {
     cy.request({
-      method: 'GET',
-      url: 'https://www.mercadolibre.com.ar/menu/departments'
+      method: "GET",
+      url: "https://www.mercadolibre.com.ar/menu/departments",
     }).then((response) => {
-      expect(response.body).to.have.property('departments')
-    })
-  })
-})
-
+      expect(response.body).to.have.property("departments");
+    });
+  });
+});
